6bb259547a25307fba999867707446752d4065dc
Shivam <goel.shivam.1998@gmail.com>
2024-03-20 05:16:17 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from './Components/Login-Signup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
